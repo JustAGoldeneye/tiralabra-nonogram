87df50f0742c2c28fae1095967b8b69807ecb43c
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2020-03-13 21:38:45 +0200

    Setting up style and test checking

--- tiralabra-nonogram/src/main/java/main/Main.java
+++ tiralabra-nonogram/src/main/java/main/Main.java
@@ -1,8 +1,16 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Sanity check
-        System.out.println("Working");
+        sanityCheck();
+    }
+    
+    private static void sanityCheck() {
+        System.out.println("Is this working? ");
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.next();
+        System.out.println("OK, " + answer);
     }
 }
