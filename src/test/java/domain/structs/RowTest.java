4f15b98014d48c9083ef99b6f5d66ce230743dbf
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2020-04-03 21:00:32 +0300

    Added JUnit test for subRow in class Row

--- src/test/java/domain/structs/RowTest.java
+++ src/test/java/domain/structs/RowTest.java
@@ -9,9 +9,20 @@ public class RowTest {
     
     @Before
     public void setUp() {
-        
+        this.row = new Row(new NumberRow(new int[]{1, 2}),
+                new SquareStatus[]{SquareStatus.BLACK, SquareStatus.CROSS,
+                    SquareStatus.EMPTY, SquareStatus.BLACK, SquareStatus.EMPTY});
     }
     
-    //TODO
-    //Test subRow
+    @Test
+    public void subRowGetsTakenCorrectly() {
+        this.row = this.row.subRow(2, 5);
+        if (this.row.lookSquareStatus(0) != SquareStatus.EMPTY) {
+            fail("Square 0 in the sub row was incorrect.");
+        } else if (this.row.lookSquareStatus(1) != SquareStatus.BLACK) {
+            fail("Square 1 in the sub row was incorrect.");
+        } else if (this.row.lookSquareStatus(2) != SquareStatus.EMPTY) {
+            fail("Square 2 in the sub row was incorrect.");
+        }
+    }
 }
